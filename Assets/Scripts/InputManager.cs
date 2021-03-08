a03b44211e2961f9f0400159742d40c87ff50582
Junsu Park <somadubel@gmail.com>
2021-03-08 17:06:18 +0900

    Practice1

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+
 public class InputManager : MonoBehaviour
 {
     public GameObject AR_object;
@@ -22,6 +23,9 @@ public class InputManager : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             Ray ray = AR_Camera.ScreenPointToRay(Input.mousePosition);
+            // Raycast(Ray, List<ARRaycastHit>, ) 
+            // : 추적가능한 물체(plane과 같은)에 Ray를 쏜다.
+            // true : rarycast가 trakbalteTypes의 tarckable에 hit한 경우.
             if(raycastManager.Raycast(ray, hits))
             {
                 Pose pose = hits[0].pose;
@@ -37,3 +41,62 @@ public class InputManager : MonoBehaviour
         }
     }
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using UnityEngine.XR.ARFoundation;
+
+// public class InputManager : MonoBehaviour
+// {
+//     public GameObject AR_object;
+//     public Camera AR_Camera;
+//     public ARRaycastManager raycastManager;
+//     public List<ARRaycastHit> hits = new List<ARRaycastHit>();
+//     public ARPlaneManager aRPlaneManager;
+
+//     [Tooltip("The UI Text element used to display plane detection messages.")]
+//     [SerializeField]
+//     Text m_TogglePlaneDetectionText;
+
+//     public Text togglePlaneDetectionText
+//     {
+//         get {return m_TogglePlaneDetectionText; }
+//         set {m_TogglePlaneDetectionText = value; }
+//     }
+
+    
+//     public void TogglePlaneDetection()
+//     {
+//         aRPlaneManager.enabled = !aRPlaneManager.enabled;
+        
+//         string planeDetectionMessage = "";
+//         if (aRPlaneManager.enabled)
+//         {
+//             planeDetectionMessage = "Disable Plane Detection and Hide Existing";
+//             // SetAllPlanesActive(true);
+//         }
+//         else 
+//         {
+//             planeDetectionMessage = "Enable Plane Detection and Show Existing";
+//             // SetAllPlanesActive(false);
+//         }
+
+//         if (togglePlaneDetectionText != null)
+//             togglePlaneDetectionText.text = planeDetectionMessage;
+
+//     }
+
+//     void SetAllPlanesActive(bool value)
+//     {
+//         foreach (var plane in aRPlaneManager.trackables)
+//             plane.gameObject.SetActive(value);
+//     }
+
+//     void Awake()
+//     {
+//         aRPlaneManager = raycastManager.GetComponent<ARPlaneManager>();
+//     }
+// }
+
